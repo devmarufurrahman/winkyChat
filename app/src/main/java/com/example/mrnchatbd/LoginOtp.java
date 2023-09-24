0964aa71746768f30168ac58a574a0ee7b7c4c06
devmarufurrahaman <mdmarufur5682@gmail.com>
2023-09-24 17:15:40 +0600

    connect to fire base

---
+++ app/src/main/java/com/example/mrnchatbd/LoginOtp.java
@@ -0,0 +1,26 @@
+package com.example.mrnchatbd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginOtp extends AppCompatActivity {
+    String phoneNumber;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_otp);
+        phoneNumber = getIntent().getExtras().getString("phone");
+        Toast.makeText(this, phoneNumber, Toast.LENGTH_SHORT).show();
+
+        Map<String,String> data = new HashMap<>();
+
+        FirebaseFirestore.getInstance().collection("test").add(data);
+    }
+}
--- app/src/main/java/com/example/mrnchatbd/LoginWithPhone.java
+++ app/src/main/java/com/example/mrnchatbd/LoginWithPhone.java
@@ -1,14 +1,54 @@
 package com.example.mrnchatbd;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppLocalesMetadataHolderService;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.hbb20.CountryCodePicker;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginWithPhone extends AppCompatActivity {
-
+    CountryCodePicker countryCodePicker;
+    EditText mobileInput;
+    Button sendOtpBtn;
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_with_phone);
+        // id define
+        countryCodePicker = findViewById(R.id.loginCountryCode);
+        mobileInput = findViewById(R.id.loginMobileInput);
+        sendOtpBtn = findViewById(R.id.sendOtpBtn);
+        progressBar = findViewById(R.id.progressPhone);
+
+
+        countryCodePicker.registerCarrierNumberEditText(mobileInput);
+        sendOtpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                if (!countryCodePicker.isValidFullNumber()){
+                    mobileInput.setError("Phone number is not Valid");
+                    progressBar.setVisibility(View.GONE);
+                    return;
+                }
+
+                Intent intent = new Intent(LoginWithPhone.this, LoginOtp.class);
+                intent.putExtra("phone",countryCodePicker.getFullNumberWithPlus());
+
+                startActivity(intent);
+
+            }
+        });
     }
 }
