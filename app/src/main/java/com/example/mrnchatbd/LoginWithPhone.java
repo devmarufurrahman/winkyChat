a769cc83703621044284ec5bd2cf1d969ed42d26
devmarufurrahaman <mdmarufur5682@gmail.com>
2023-09-24 13:55:17 +0600

    login page design

---
+++ app/src/main/java/com/example/mrnchatbd/LoginWithPhone.java
@@ -0,0 +1,14 @@
+package com.example.mrnchatbd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginWithPhone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_with_phone);
+    }
+}
